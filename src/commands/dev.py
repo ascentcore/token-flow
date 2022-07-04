0b6c68952f512ad08ed72c2d996ca49cd8748562
Cornel Stefanache <cornel.stefanache@gmail.com>
2022-07-04 10:07:50 +0300

    better layouting

--- src/commands/dev.py
+++ src/commands/dev.py
@@ -1,5 +1,6 @@
 import logging
 from operator import sub
+from random import random
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
@@ -7,7 +8,8 @@ import spacy
 import imageio
 
 nlp = spacy.load("en_core_web_sm")
-figure(figsize=(8, 6), dpi=300)
+plt = figure(figsize=(8, 6), dpi=150)
+
 logger = logging.getLogger(__name__)
 
 # if key not in visited:
@@ -28,6 +30,9 @@ logger = logging.getLogger(__name__)
 #             stimulate(sub_key, weight * new_value, G, visited, nodes=nodes)
 
 # return visited
+
+
+positions = {}
 
 
 def do_plot(G, writer, pos=None, title=None):
@@ -56,8 +61,13 @@ def do_plot(G, writer, pos=None, title=None):
         edge_width.append(edge[2]['weight'] * 0.5)
 
     if pos is None:
-        # pos = nx.spring_layout(subgraph, k=0.15, iterations=100)
-        pos = nx.kamada_kawai_layout(subgraph)
+        # pos = nx.fruchterman_reingold_layout(subgraph)
+        pos = nx.spring_layout(subgraph, k=0.5,   iterations=100)
+        for p_key in pos.keys():
+            if p_key in positions:
+                pos[p_key] = positions[p_key]
+            else:
+                positions[p_key] = pos[p_key]
 
     nx.draw_networkx_nodes(
         subgraph, pos=pos, node_size=node_size, node_color='none', edgecolors='red', linewidths=0.1)
@@ -65,13 +75,15 @@ def do_plot(G, writer, pos=None, title=None):
                             verticalalignment='bottom')
     nx.draw_networkx_edges(subgraph, pos, width=edge_width)
     ax = plt.gca()
+    ax.set_xlim([-1.2, 1.2])
+    ax.set_ylim([-1.2, 1.2])
     ax.set_title(title)
     plt.savefig('output/graph.jpg')
     plt.clf()
     image = imageio.imread('output/graph.jpg')
     writer.append_data(image)
 
-    nx.write_gml(subgraph, 'output/graph.gml')
+    # nx.write_gml(subgraph, 'output/graph.gml')
 
 
 def stimulate(key, value, G, factor=1, to_set=None, nodes=None):
@@ -85,7 +97,7 @@ def stimulate(key, value, G, factor=1, to_set=None, nodes=None):
     if key not in to_set:
         node = G[key]
         previous_value = nodes[key]['s']
-        new_value = min(1, previous_value + value)
+        new_value = previous_value + value
         to_set[key] = {'s':  new_value}
 
         if new_value > 0.1:
@@ -93,7 +105,7 @@ def stimulate(key, value, G, factor=1, to_set=None, nodes=None):
                 if sub_key != key:
                     weight = node[sub_key]['weight']
                     stimulate(sub_key, weight * new_value,
-                              G, factor * 0.5, to_set=to_set, nodes=nodes)
+                              G, factor * 0.75, to_set=to_set, nodes=nodes)
 
     return to_set
 
@@ -101,9 +113,6 @@ def stimulate(key, value, G, factor=1, to_set=None, nodes=None):
 def execute(args):
     G = nx.read_edgelist("graphs/full-dictionary.edgelist")
     G.remove_edges_from(nx.selfloop_edges(G))
-    # pos = nx.spring_layout(G, k=0.15, iterations=100)
-    # pos = nx.kamada_kawai_layout(G)
-    # pos = nx.random_layout(G)
     pos = None
 
     nx.set_node_attributes(G, 0, name='s')
@@ -114,31 +123,31 @@ def execute(args):
 
     doc = nlp(test_text.lower())
     sentences = str(doc).splitlines()
+    relink = False
     with imageio.get_writer('output/output.gif', mode='I') as writer:
         for sentence in sentences:
             if sentence != '':
                 to_link = []
                 rest_tokens = []
                 tokens = nlp(sentence)
+                to_set = {}
                 for token in tokens:
                     if token.pos_ == "NOUN":
                         if not G.has_node(token.lemma_):
-                            G.add_node(token.lemma_, s=0)
+                            node = G.add_node(token.lemma_, s=0)
                             to_link.append(token.lemma_)
 
                         if token.lemma_ not in rest_tokens:
                             rest_tokens.append(token.lemma_)
 
-                        to_set = stimulate(token.lemma_, 1, G)
+                        to_set = stimulate(token.lemma_, 0.5, G, to_set=to_set)
                         nx.set_node_attributes(G, to_set)
 
-                    elif token.pos == "PROPN":
-                        print(token.pos_)
-
-                for token in to_link:
-                    for to_token in rest_tokens:
-                        if token != to_token:
-                            G.add_edge(token, to_token, weight=0.1)
+                if relink:
+                    for token in to_link:
+                        for to_token in rest_tokens:
+                            if token != to_token:
+                                G.add_edge(token, to_token, weight=0.1)
 
                 list = G.nodes(data=True)
                 set_values = {}
@@ -147,26 +156,26 @@ def execute(args):
                     set_values[node] = {'s': max(0, new_value)}
                 nx.set_node_attributes(G, set_values)
 
-                #We should create connections between nodes that are strongly stimulated
-                nodes = (
-                    node
-                    for node, data
-                    in G.nodes(data=True)
-                    if data.get("s") > 0.5
-                )
+                # We should create connections between nodes that are strongly stimulated
 
-                subgraph = G.subgraph(nodes)
-                nodes_list = subgraph.nodes()
+                if relink:
+                    nodes = (
+                        node
+                        for node, data
+                        in G.nodes(data=True)
+                        if data.get("s") > 0.1
+                    )
 
-                if len(nodes_list) > 1:
-                    
-                    for node_i in nodes_list:
-                        for node_j in nodes_list:
-                            if node_i != node_j and not G.has_edge(node_i, node_j):
-                                print(node_i, node_j)
-                                G.add_edge(node_i, node_j, weight=0.1)
+                    subgraph = G.subgraph(nodes)
+                    nodes_list = subgraph.nodes()
+
+                    if len(nodes_list) > 1:
+
+                        for node_i in nodes_list:
+                            for node_j in nodes_list:
+                                if node_i != node_j and not G.has_edge(node_i, node_j):
+                                    print(node_i, node_j)
+                                    G.add_edge(node_i, node_j, weight=0.1)
                 # End of we should create :)
-                
-                
 
                 do_plot(G, writer, pos, title=sentence)
