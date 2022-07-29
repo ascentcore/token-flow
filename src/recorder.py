429295fbdce15872c4a2f9bc63c43ca9b1f9870a
Cornel Stefanache <cornel.stefanache@gmail.com>
2022-07-29 09:16:20 +0300

    added kids dictionary

--- src/recorder.py
+++ src/recorder.py
@@ -6,18 +6,32 @@ class Recorder(Context):
 
     writer = None
 
+    recording = False
+
     def stimulate(self, *args, **kwargs):
         super().stimulate(*args, **kwargs)
         if self.writer and 'to_set' not in kwargs.keys():
             self.capture_frame()
 
+    def add_definition(self, *args, **kwargs):
+        res = super().add_definition(*args, **kwargs)
+        self.capture_frame()
+        return res
+
+    def add_text(self, *args, **kwargs):
+        res = super().add_text(*args, **kwargs)
+        self.capture_frame()
+        return res
+
     def capture_frame(self):
-        self.render('./frame.png', self.title, self.consider_stimulus, self.arrow_size)
-        image = imageio.imread('./frame.png')
-        self.writer.append_data(image)
+        if self.recording:
+            self.render('./frame.png', self.title, self.consider_stimulus, self.arrow_size)
+            image = imageio.imread('./frame.png')
+            self.writer.append_data(image)
 
     def start_recording(self, filename, title, consider_stimulus, fps = 12, arrow_size = 3):
         self.title = title
+        self.recording = True
         self.consider_stimulus = consider_stimulus
         self.arrow_size = arrow_size
         self.writer = imageio.get_writer(filename, mode='I', fps=fps)
@@ -25,4 +39,5 @@ class Recorder(Context):
 
     def stop_recording(self):
         self.writer.close()
+        self.recording = False
         self.write = None
---
+++ tests/showcase_test.py
@@ -0,0 +1,67 @@
+from src.recorder import Recorder
+from src.context import Context
+from src.vocabulary import Vocabulary
+
+import unittest
+import json
+
+
+record = True
+
+class TestShowcase(unittest.TestCase):
+
+    def test_showcase(self):
+
+        if record:
+            context = Recorder('Showcase', include_start=False, initial_weight=0.2)
+        else:
+            context = Context('Showcase', include_start=False, initial_weight=0.2)
+
+
+        if record:
+            context.start_recording('output/tests/output.gif',
+                                    title="Test Gif", consider_stimulus=False, fps=3, arrow_size=0.5)
+                            
+        
+        context.add_definition(
+            'salad', 'a mixture of cold vegetables such as lettuce, tomato, and cucumber, served with a dressing.')
+
+        context.add_definition(
+            'lettuce', 'a plant with large, crisp leaves that can be eaten.'
+        )
+
+        context.add_definition('tomato', 'a red or yellow fruit with a juicy pulp. A tomato is eaten either raw or cooked as a vegetable.')
+        context.add_definition('cucumber', 'a plant with a green, round, fleshy fruit that is used in salads, soups, and other dishes.')
+        context.add_definition('vegetbale', 'a plant or part of a plant, such as carrots, beans, or lettuce, that is used for food.')
+
+        # context.add_definition('sausage', 'a mixture of chopped meat and spices stuffed into a casing of animal intestine.')
+
+
+        context.add_definition('favorite', 'food is salad')
+        context.add_definition('greek', 'salad olive oil, tomatoes, lettuce, and cucumber')
+        context.add_definition('tomato', 'a red or yellow fruit with a juicy pulp')
+        context.add_definition('sausage', 'a mixture of chopped meat and spices stuffed')
+        context.add_definition('eat', 'the process of eating food')
+
+        context.add_text('my favorite food are meats')
+        context.add_text('my favorite food are fruits')
+        context.add_text('I like greek salad')
+        context.add_text('my prefference in food are vegetables')
+        context.add_text('I do not eat meat')
+
+        
+      
+        context.consider_stimulus = True
+
+        context.stimulate_sequence('I am very hungry, I need to eat, I think I need a salad or something lite')
+        
+        context.render('output/tests/output.png', consider_stimulus=True)
+
+        if record:
+            context.stop_recording()
+
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
