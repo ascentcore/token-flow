7a2df57240efd44ec0f43b90028e509bb56c5e32
Cornel Stefanache <cornel.stefanache@gmail.com>
2022-12-13 17:52:29 +0200

    fixed loading

--- studies/text/config.py
+++ studies/text/config.py
@@ -16,30 +16,33 @@ size = 11
 history = 5
 next = 5
 
-vocabulary = Vocabulary.from_file('studies/text/dataset')
+def get_training_setup():
+    contexts = {}
+    vocabulary = Vocabulary.from_file('studies/text/dataset')
 
-config = CfgNode()
-config.model_type = None
-config.vocab_size = len(vocabulary.vocabulary)
-config.block_size = size
-config.n_embd = n_dim
-config.n_layer = 1
-config.n_head = 1
-config.embd_pdrop = 0.1
-config.attn_pdrop = 0.1
-config.resid_pdrop = 0.1
-config.betas = (0.9, 0.95)
-config.weight_decay = 0.1 # only applied on matmul weights
-config.grad_norm_clip = 1.0
-config.learning_rate = 0.001
-config.pretrained_embeddings = None
+    config = CfgNode()
+    config.model_type = None
+    config.vocab_size = len(vocabulary.vocabulary)
+    config.block_size = size
+    config.n_embd = n_dim
+    config.n_layer = 1
+    config.n_head = 1
+    config.embd_pdrop = 0.1
+    config.attn_pdrop = 0.1
+    config.resid_pdrop = 0.1
+    config.betas = (0.9, 0.95)
+    config.weight_decay = 0.1 # only applied on matmul weights
+    config.grad_norm_clip = 1.0
+    config.learning_rate = 0.001
+    config.pretrained_embeddings = None
 
-settings = json.loads(open(f'studies/text/dataset/dataset.settings.json').read())
+    settings = json.loads(open(f'studies/text/dataset/dataset.settings.json').read())
 
-contexts_list = None
-contexts = {}
+    contexts_list = None
 
-for context_name in settings['contexts']:
-    if context_name != 'default' and (contexts_list is None or context_name in contexts_list):
-        context = Context.from_file('studies/text/dataset', context_name, vocabulary)
-        contexts[context_name] = context
+    for context_name in settings['contexts']:
+        if context_name != 'default' and (contexts_list is None or context_name in contexts_list):
+            context = Context.from_file('studies/text/dataset', context_name, vocabulary)
+            contexts[context_name] = context
+
+    return contexts, vocabulary, config
